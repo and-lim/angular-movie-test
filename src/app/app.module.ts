7d67b197de5cf19490469fc885dce5a2ec1290d8
and-lim <andrianlimnardy@gmail.com>
2024-02-02 17:54:26 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import {HttpClientModule} from '@angular/common/http';
+import { MovieApiServiceService } from './service/movie-api-service.service';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MovieComponent } from './pages/movie/movie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    MovieDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [MovieApiServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
